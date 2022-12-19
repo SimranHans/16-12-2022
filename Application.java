9d7e93b6104c8f43be4dbf796130ab350a785b2a
SimranHans <120483989+SimranHans@users.noreply.github.com>
2022-12-19 14:20:02 +0530

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,18 @@
+/*
+5.	Create a Java program that simply outputs the text "Hello World" when you run it. Call your main class "Application". 
+
+In the same file as your main "Application" class, define a new class called "Person". You don't need to put anything in the class. 
+*/
+
+
+
+public class Application {
+    public static void main(String args[]){
+        System.out.println("Hello World");
+
+        class Person{
+            
+        }
+    }
+    
+}
